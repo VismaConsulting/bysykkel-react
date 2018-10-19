05e63cee6d560419dd347a12259f08a957fe6877
Frederik Gørvell de Lichtenberg <frederik.gorvell.de.lichtenberg@visma.com>
2018-10-19 15:44:55 +0200

    Add express server and deploy files

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use('/', express.static(path.join(__dirname, 'build')));
+
+app.get('/actuator/isAlive', (err, res) => {
+    res.status(200).send("Is Alive!")
+});
+
+app.get('/actuator/isReady', (err, res) => {
+    res.status(200).send("Is Ready!")
+});
+
+//var port = process.env.PORT || 80     HEROKU
+var port = process.env.PORT || 8080
+app.listen(port)
+console.log('Starting project at ' + port)
