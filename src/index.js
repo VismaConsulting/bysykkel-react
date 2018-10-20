1cd67f050cd2d33d412e3596d0aa3cc4beb3201d
Flogstad <peter.flogstad@gmail.com>
2018-10-20 14:24:34 +0200

    Endre api url

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, Route, Link } from 'react-router';
 import configureStore from './store';
 
 import './index.css';
 import App from './App';
-import MinSide from './MinSide';
 
 const store = configureStore();
 
