8b358f61e2d8d937411feeebd8681d849c3b6fea
Solveig Maria Nes <solveig.maria.nes@visma.com>
2018-10-20 15:56:39 +0200

    Lagt til space etter challenge, litt lengre animasjon

--- src/components/SelectedChallenge.js
+++ src/components/SelectedChallenge.js
@@ -8,10 +8,10 @@ export const SelectedChallenge = ({ station, challenge }) => {
     const {type, points} = challenge;
 
     const description = type === 'HENT' ?
-        `Hent en sykkel fra dette stativet for å få ${points} poeng`
-        : `Lever en sykkel til dette stativet for å få ${points} poeng` ;
+        ` Hent en sykkel fra dette stativet for å få ${points} poeng`
+        : ` Lever en sykkel til dette stativet for å få ${points} poeng` ;
 
     return (
-        <div><span className="bold">Challenge: </span>{description}</div>
+        <div><span className="bold">Challenge:</span>{description}</div>
     );
 };
